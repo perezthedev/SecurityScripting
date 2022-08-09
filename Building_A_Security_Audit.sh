42abdd0d5a251cc0f7cc5d7a602e7272d10cfb3c
Perez The Dev <49325984+perezthedev@users.noreply.github.com>
2022-08-08 19:44:04 -0700

    Add files via upload
    increased user readability using control flows

--- Building_A_Security_Audit.sh
+++ Building_A_Security_Audit.sh
@@ -19,6 +19,7 @@ sysModel=$(osqueryi 'SELECT hardware_model FROM system_info')
 echo "${sysModel}"
 macAddress=$(osqueryi 'SELECT mac AS MAC_address FROM interface_details')
 echo "${macAddress}"
+
 listOfUsers=$(osqueryi 'SELECT DISTINCT user AS Users FROM logged_in_users')
 echo "${listOfUsers}"
 usersUUID=$(osqueryi 'SELECT uuid AS UUID FROM users')
@@ -27,8 +28,22 @@ echo "${usersUUID}"
 #echo "${lastLogin}"
 #lastLogout=$(osqueryi --json --header=FALSE --list 'SELECT * FROM last')
 #echo "${lastLogout}"
-sipStatus=$(osqueryi --json --header=true --list 'SELECT enabled AS SIP_Status FROM sip_config WHERE config_flag="sip"')
-echo "${sipStatus}"
+
+sipStatus=$(osqueryi --json --header=false --list 'SELECT enabled AS SIP_Status FROM sip_config WHERE config_flag="sip"')
+# make System Integrity Protection status more human readable
+if [ "${sipStatus}" == 1 ]
+then
+    sipStatus="Enabled"
+else
+    sipStatus="Disabled"
+fi
+
+echo "+------------+"
+echo "| SIP_Status "
+echo "+------------+"
+echo "| ${sipStatus}"
+echo "+------------+"
+
 anyUpdatesAvailable=$(osqueryi 'SELECT * FROM plist WHERE path = "/Library/Preferences/com.apple.commerce.plist" and key = "AutoUpdate"')
 echo "${anyUpdatesAvailable}"
 uptimeDays=$(osqueryi 'SELECT days FROM uptime')
@@ -43,11 +58,42 @@ echo "${uptimeSeconds}"
 #echo "${uptimeTotalInSeconds}"
 xprotectID=$(osqueryi 'SELECT name AS XProtected_Entries FROM xprotect_entries')
 echo "${xprotectID}"
-firewallStatus=$(osqueryi 'SELECT global_state AS Firewall_Status FROM alf')
-echo "${firewallStatus}"
-gateKeeper=$(osqueryi 'SELECT assessments_enabled AS Gatekeeper_Status FROM gatekeeper')
-echo "${gateKeeper}"
-diskEncryption=$(osqueryi 'SELECT name, encrypted FROM disk_encryption')
-echo "${diskEncryption}"
+firewallStatus=$(osqueryi --header=false --list 'SELECT global_state AS Firewall_Status FROM alf')
+
+if [ "${firewallStatus}" == 1 ]
+then
+    firewallStatus="Enabled"
+else
+    firewallStatus="Disabled"
+fi
+
+echo "+-----------------+"
+echo "| Firewall_Status |"
+echo "+-----------------+"
+echo "| ${firewallStatus}"
+echo "+-----------------+"
+
+gateKeeper=$(osqueryi --header=false --list 'SELECT assessments_enabled AS Gatekeeper_Status FROM gatekeeper')
+
+if [ "${gateKeeper}" == 1 ]
+then
+    gateKeeper="Enabled"
+else
+    gateKeeper="Disabled"
+fi
+
+echo "+-------------------+"
+echo "| Gatekeeper_Status |"
+echo "+-------------------+"
+echo "| ${gateKeeper}"
+echo "+-------------------+"
+
+diskEncryption=$(fdesetup status)
+echo "+----------------------+"
+echo "| Encryption_Status    |"
+echo "+----------------------+"
+echo "| ${diskEncryption}"
+echo "+----------------------+"
+
 usbDevices=$(osqueryi 'SELECT model AS USB_Model FROM usb_devices')
 echo "${usbDevices}"

bc8bbe483d34ac8f1da5ef4d188a17cb1f411a01
Perez The Dev <49325984+perezthedev@users.noreply.github.com>
2022-12-17 23:36:59 -0800

    Update defang.py
    how to defang a given IP address (no exception handling)

--- defang.py
+++ defang.py
@@ -1,17 +1,3 @@
-import re
-
-regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
-
-def check(Ip):
- 
-    # pass the regular expression
-    # and the string in search() method
-    if(re.search(regex, Ip)):
-        print("Valid Ip address")
-         
-    else:
-        print("Invalid Ip address")
-
 def defang(ip: str) -> str:
     ans = ''
     for char in ip:
@@ -21,9 +7,7 @@ def defang(ip: str) -> str:
             ans += char
     return ans 
     #return ip.replace(".", "[.]")
-validInput = False
-
-while not validInput:
-    testIP = input("Enter the desired IP address:")
+    
+testIP = input("Enter the desired IP address:")
 
 print("Defanged:", defang(testIP))
